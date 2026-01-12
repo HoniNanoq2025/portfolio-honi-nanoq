eb1362cd1929e2a11a8dbd61df938f9eb42ffe54
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2026-01-12 13:25:09 +0100

    create Home page, update Button to express props, and update css and index.html

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,72 @@
+import { useNavigate, Link } from "react-router-dom";
+import profilePic from "../../assets/img/profile-pic-02-400x400.png";
+import Button from "../../components/Button/Button";
 import styles from "./Home.module.css";
 
-export default function Home() {}
+export default function Home() {
+  const navigate = useNavigate(); // Hook til navigation
+
+  const handleNavigateAbout = () => {
+    navigate("/about");
+  };
+
+  const handleNavigateProjects = () => {
+    navigate("/projects");
+  };
+
+  const handleNavigateResume = () => {
+    navigate("/resume");
+  };
+
+  const handleNavigateContact = () => {
+    navigate("/contact");
+  };
+
+  return (
+    <div className={styles.home}>
+      {/* H1 header - font: Island Moments */}
+      <div className={styles.header}>
+        <h1>
+          Welcome <br /> to
+        </h1>
+      </div>
+      {/* H2 header - font: Irish Grover */}
+      <div className={styles.subHeader}>
+        <h2>
+          THE PORTFOLIO AND RESUMÉ OF <br /> HONI
+        </h2>
+      </div>
+      {/* Profile image - leads to About page */}
+      <div className={styles.aboutContainer}>
+        <Link to="/about">
+          <img
+            src={profilePic}
+            alt="About Honi - link"
+            width={250}
+            className={styles.aboutImgLink}
+          />
+        </Link>
+      </div>
+      <div className={styles.buttonContainer}>
+        <Button
+          typeButton="button"
+          onButtonClick={() => handleNavigateProjects}
+          buttonText="MY PROJECTS"
+          btnSize={styles.btnSize}
+        />
+        <Button
+          typeButton="button"
+          onButtonClick={() => handleNavigateResume}
+          buttonText="RESUMÉ"
+          btnSize={styles.btnSize}
+        />
+        <Button
+          typeButton="button"
+          onButtonClick={() => handleNavigateContact}
+          buttonText="CONTACT"
+          btnSize={styles.btnSize}
+        />
+      </div>
+    </div>
+  );
+}
