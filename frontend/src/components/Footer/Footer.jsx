b9587bc33f8e0b26f073de9587eeaef5bc578862
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2026-01-12 12:20:43 +0100

    update Header, Footer and css

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,3 +1,19 @@
-import styles from "./Footer.module.css"
+import { RxLinkedinLogo } from "react-icons/rx";
+import styles from "./Footer.module.css";
 
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://www.linkedin.com/in/hejdi-nielsen-2a1aa153"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <RxLinkedinLogo size={36} color="white" />
+      </a>
+      <p className={styles.footerText}>
+        &copy; Copyright 2025 - Honi Nanoq&trade;
+      </p>
+    </footer>
+  );
+}
