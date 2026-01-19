6e0ef3b6766066f77bb7b06013dc535d5b31ff9a
Hejdi Østergaard Nielsen <195737596+HoniNanoq2025@users.noreply.github.com>
2026-01-19 11:38:09 +0100

    Start coding About

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,3 +1,21 @@
+import ImgHoni from "../../assets/img/profile-pic-01-400x400.png";
 import styles from "./About.module.css";
 
-export default function About() {}
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <div className={styles.aboutHeader}>
+        <h1>About</h1>
+      </div>
+      <div className={styles.aboutImgHoni}>
+        <img
+          src={ImgHoni}
+          alt="Image of Honi in drawing style"
+          height={200}
+          className={styles.aboutHoniPic}
+        />
+        <div className={styles.imgOverlay}>HONI</div>
+      </div>
+    </div>
+  );
+}
