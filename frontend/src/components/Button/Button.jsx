5136ad9a67d68b7a0d02c4ee97a86e004fe2f5f0
Hejdi Østergaard Nielsen <195737596+HoniNanoq2025@users.noreply.github.com>
2026-01-28 20:14:20 +0100

    update Button

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -4,10 +4,11 @@ export default function Button(props) {
   return (
     <button
       type={props.typeButton || "button"}
-      className={`${styles.button} ${props.btnSize || ""}`}
+      className={`${styles.button} ${props.btnSize || ""} ${props.className || ""}`}
       onClick={props.onButtonClick}
     >
-      {props.buttonText}
+      {props.icon && <span className={styles.icon}>{props.icon}</span>}
+      {props.buttonText || props.children}
     </button>
   );
 }
