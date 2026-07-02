7cf834b4253ee36be2bdc851d2158658925c74d3
Hejdi Østergaard Nielsen <195737596+HoniNanoq2025@users.noreply.github.com>
2026-07-03 01:14:02 +0200

    add spacing

--- frontend/src/pages/Projects/Projects.jsx
+++ frontend/src/pages/Projects/Projects.jsx
@@ -35,7 +35,7 @@ export default function Projects() {
             >
               YouTube demo
             </a>
-            |
+            | 
             <a
               href={PodcastCaseStudyDK}
               className={styles.gitHubLink}
